c962934c3169f1df978c63494503083843c1dd02
vvvwqmohened-lang <vvvwqmohened@gmail.com>
2026-05-12 02:53:12 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,12 @@
 // ========== إعدادات Firebase ==========
 const firebaseConfig = {
     apiKey: "AIzaSyBygVOgLelAsR0k0MiH6CoQAAuVPKX", // استبدل
-     authDomain: "myproject-b6353.firebaseapp.com",
-           
-                projectId: "myproject-b6353",
-                storageBucket: "myproject-b6353.firebasestorage.app",
-                messagingSenderId: "339621647444",
-                appId: "1:339621647444:web:8e0becdac3a46350d549c1"
-            };
-};
+     authDomain: "myproject-b6353.firebaseapp.com", 
+     projectId: "myproject-b6353",
+     storageBucket: "myproject-b6353.firebasestorage.app",
+     messagingSenderId: "339621647444",
+     appId: "1:339621647444:web:8e0becdac3a46350d549c1"
+  };
 
 // تهيئة Firebase
 firebase.initializeApp(firebaseConfig);
